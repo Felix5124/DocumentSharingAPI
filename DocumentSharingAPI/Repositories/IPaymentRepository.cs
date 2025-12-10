6f05664a3de64637702d0e9cd92b0f14bdea5419
dajtkw <buithanhdat1462004@gmail.com>
2025-12-10 22:10:17 +0700

    Pagination and Searching in Payment Management

--- DocumentSharingAPI/Repositories/IPaymentRepository.cs
+++ DocumentSharingAPI/Repositories/IPaymentRepository.cs
@@ -9,5 +9,14 @@ namespace DocumentSharingAPI.Repositories
         Task<List<Payment>> GetPaymentsByUserIdAsync(int userId);
         Task<List<Payment>> GetExpiredPaymentsAsync();
         Task<Payment?> GetPaymentWithUserAsync(int paymentId);
+        
+        // Thêm hàm này
+        Task<(List<Payment> Items, int TotalCount)> GetAdminPaymentsAsync(
+            int page,
+            int pageSize,
+            string keyword,
+            string status,
+            DateTime? fromDate,
+            DateTime? toDate);
     }
 }
