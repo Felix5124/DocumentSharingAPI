648339bd62c0de7e4fd8c67044c3cab20c5bf2b7
Felix5124 <khoanguyen3633@gmail.com>
2025-12-12 22:44:44 +0700

    Update UserController

---
+++ DocumentSharingAPI/Migrations/20251212144839_AddUserSettings.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentSharingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Settings",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Settings",
+                table: "Users");
+        }
+    }
+}
