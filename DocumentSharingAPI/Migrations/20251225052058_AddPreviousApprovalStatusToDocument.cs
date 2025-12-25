06f5eef43e6ad7eaeed316fb5cd753076a51ca10
hungzk124@gmail.com <hungzk124@gmail.com>
2025-12-25 13:01:49 +0700

    Update report count

---
+++ DocumentSharingAPI/Migrations/20251225052058_AddPreviousApprovalStatusToDocument.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentSharingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPreviousApprovalStatusToDocument : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PreviousApprovalStatus",
+                table: "Documents",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PreviousApprovalStatus",
+                table: "Documents");
+        }
+    }
+}
